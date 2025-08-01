d141fc39e3bec936f076aeb14b1f6fc7e615d6f1
Mykyta_Rymar <mykitarymar@gmail.com>
2025-08-01 03:50:11 +0200

    12341234йцук1234

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,3 +1,8 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
 const startBtn = document.querySelector('.input-btn');
 const dateInput = document.querySelector('#datetime-picker');
 const daysEl = document.querySelector('[data-days]');
